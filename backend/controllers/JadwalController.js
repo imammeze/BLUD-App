045debffc29418b93f72f0dd6827104b00d93c88
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2025-05-05 20:45:19 +0700

    menambahkan update dan delete JadwalController

--- backend/controllers/JadwalController.js
+++ backend/controllers/JadwalController.js
@@ -43,6 +43,51 @@ export const createJadwal = (req, res) => {
   }
 };
 
-export const updateJadwal = (req, res) => {};
+export const updateJadwal = async (req, res) => {
+  const { name, name_event, tanggal } = req.body;
+  const tanggalObjek = new Date(tanggal);
 
-export const deleteJadwal = (req, res) => {};
+  const jadwal = await Jadwal.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (!jadwal) return res.status(404).json({ msg: "Data tidak ditemukan" });
+
+  if (isNaN(tanggalObjek)) {
+    return res.status(400).json({ msg: "Format tanggal tidak valid" });
+  }
+
+  try {
+    Jadwal.update({
+      name,
+      name_event,
+      tanggal: tanggalObjek,
+    });
+    res.status(201).json({ msg: "Jadwal berhasil diubah" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Gagal mengubah jadwal" });
+  }
+};
+
+export const deleteJadwal = async (req, res) => {
+  const jadwal = await Jadwal.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (!jadwal) return res.status(404).json({ msg: "Data tidak ditemukan" });
+
+  try {
+    await Jadwal.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ msg: "Jadwal berhasil dihapus" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Gagal menghapus Jadwal" });
+  }
+};
